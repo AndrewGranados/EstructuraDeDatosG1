9455a12419badb57a4ab001811cfba74cfd664af
Aleks Andrew Garcia <aleks.garcia.009@gmail.com>
2024-09-06 08:47:44 -0600

    Logro: Se realizaron los programas de serie y su prueba y se comenzó el de factorial

--- 01_1-Conceptos_basicos_vs/01-estructuras-iterativas/src/edu/mx/uttt/ciclos/Serie.java
+++ 01_1-Conceptos_basicos_vs/01-estructuras-iterativas/src/edu/mx/uttt/ciclos/Serie.java
@@ -11,7 +11,6 @@ public class Serie {
 
     public Serie(int n) {
         this.n = (n <= 0) ? 1 : n;
-
         /*
          * if (n>0) {
          * this.n = n;
@@ -32,28 +31,38 @@ public class Serie {
     public double calcularFor() {
         double r = 0.0;
         double constante = 1.0;
-        for (int i = 0; i < this.n; i++) {
+        for (int i = 1; i < this.n; i++) {
             r += constante / i;
         }
         return r;
-    
     }
+
     public double calcularWhile() {
         double r = 0.0;
         double constante = 1.0;
         double i = 1.0;
-        while(i < this.n) {
+        while (i <= this.n) {
             r += constante / i;
             i++;
         }
         return r;
     }
 
+    public double caluclarDoWhile() {
+        int i = 1;
+        double r = 0.0;
+        double constante = 1.0;
+        do {
+            r += constante / i;
+            i++;
+        } while (i <= this.n);
 
+        return r;
+    }
 
     @Override
     public String toString() {
-        return "Serie [n=" + n + "]";
+        return "Serie [n = " + n + "]";
     }
 
 }
